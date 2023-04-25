e6fa83c951b0487de963e5a49d5ec557a5cd247b
jidoil <dotteda@gmail.com>
2023-04-26 01:22:48 +0900

    camplist

--- pages/makeNewCamp.js
+++ pages/makeNewCamp.js
@@ -1,12 +1,31 @@
-import NavBar from "../components/NavBar";
-import Head from "next/head";
+import {useState} from "react";
 
 function MakeNewCamp() {
     console.log('About Render')
+    const [formData, setFormData] = useState({});
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const formData = new FormData(form);
+
+        const jsonData = {};
+        for (const [key, value] of formData.entries()) {
+            jsonData[key] = value;
+        }
+
+        // JSON 데이터를 상태에 저장 또는 서버로 전송 등의 작업 수행
+        setFormData(jsonData);
+
+    };
+    console.log(formData)
+
     return (
         <>
             <div className="flex justify-center">
-                <form className="w-full max-w-lg" action="/camps" method="post">
+                <form className="w-full max-w-lg" action="/camps" method="post" onSubmit={handleSubmit}>
+
                     {/*부트 캠프 이름*/}
                     <div className="flex flex-wrap -mx-3 mb-6 mt-6">
                         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -133,6 +152,7 @@ function MakeNewCamp() {
                             </ul>
                         </div>
                     </div>
+
                     {/*부트 캠프 시작일*/}
                     <div className="flex flex-wrap -mx-3 mb-6 mt-6">
                         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -174,16 +194,169 @@ function MakeNewCamp() {
                         </div>
                     </div>
 
-                    {/*부트 캠프 과정*/}
+                    {/* 수업 방식 */}
                     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                         <p className="mb-4 font-semibold text-gray-700 dark:text-white text-xs">파트타임</p>
                         <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                             <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
                                 <div className="flex items-center pl-3">
-                                    <input id="parttime-checkbox-list" type="checkbox" value="parttime"
+                                    <input id="parttime-checkbox-list" name="parttime-checkbox-list" type="checkbox" value="파트타임"
                                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
                                     <label htmlFor="parttime-checkbox-list"
                                            className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">풀타임</label>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    {/* 수업 방식 */}
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <p className="mb-4 font-semibold text-gray-700 dark:text-white text-xs">수업 방식</p>
+                        <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="course-type" name="course-type" type="checkbox" value="onlinelive"
+                                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="course-type"
+                                           className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">온라인 라이브</label>
+                                </div>
+                            </li>
+                            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="course-type" name="course-type" type="checkbox" value="offlinelive"
+                                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="course-type"
+                                           className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">오프라인 라이브</label>
+                                </div>
+                            </li>
+
+                            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="course-vod" name="course-vod" type="checkbox" value="VOD"
+                                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="course-vod"
+                                           className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">VOD</label>
+                                </div>
+                            </li>
+
+                            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="course-record" name="course-record" type="checkbox" value="record"
+                                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="course-record"
+                                           className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">녹화본 제공</label>
+                                </div>
+                            </li>
+                            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="course-record" name="course-record" type="checkbox" value="no-record"
+                                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="course-record"
+                                           className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">녹화본 미제공</label>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    {/* 프로젝트 */}
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <p className="mb-4 font-semibold text-gray-700 dark:text-white text-xs">프로젝트</p>
+                        <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="project-type" name="project-type" type="checkbox" value="team"
+                                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="project-type"
+                                           className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">팀 프로젝트</label>
+                                </div>
+                            </li>
+                            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="project-type" name="project-type" type="checkbox" value="self"
+                                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="project-type"
+                                           className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">개인 프로젝트</label>
+                                </div>
+                            </li>
+                            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="project-type" name="project-type" type="checkbox" value="team"
+                                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="project-type"
+                                           className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">팀프로젝트</label>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    {/* 운영 기업 */}
+                    <div className="flex flex-wrap -mx-3 mb-6 mt-6">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-4"
+                                   htmlFor="host">
+                                운영 기업
+                            </label>
+                            <input
+                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                id="host" name="host" type="text" placeholder="운영 기업" />
+                            <p className="text-red-500 text-xs italic">입력해 주세요.</p>
+                        </div>
+                    </div>
+
+                    {/* 지원 자격 */}
+                    <div className="flex flex-wrap -mx-3 mb-6 mt-6">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-4"
+                                   htmlFor="condition">
+                                지원 자격
+                            </label>
+                            <input
+                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                id="condition" name="condition" type="text" placeholder="지원 자격" />
+                            <p className="text-red-500 text-xs italic">입력해 주세요.</p>
+                        </div>
+                    </div>
+
+                    {/* 선수 학습 */}
+                    <div className="flex flex-wrap -mx-3 mb-6 mt-6">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-4"
+                                   htmlFor="host">
+                                선수 학습
+                            </label>
+                            <input
+                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                id="pre-requisite" name="pre-requisite" type="text" placeholder="선수 학습" />
+                            <p className="text-red-500 text-xs italic">입력해 주세요.</p>
+                        </div>
+                    </div>
+
+                    {/* 선발 절차 */}
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <p className="mb-4 font-semibold text-gray-700 dark:text-white text-xs">선발 절차</p>
+                        <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="tuition" name="tuition" type="checkbox" value="coding"
+                                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="project-type"
+                                           className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">코딩 테스트</label>
+                                </div>
+                            </li>
+                            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="tuition" name="tuition" type="checkbox" value="interview"
+                                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="tuition"
+                                           className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">면접</label>
+                                </div>
+                            </li>
+                            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="tuition" name="tuition" type="checkbox" value="no-test"
+                                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="tuition"
+                                           className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">없음</label>
                                 </div>
                             </li>
                         </ul>
@@ -199,19 +372,6 @@ function MakeNewCamp() {
                     </div>
                 </form>
             </div>
-            {
-             document.querySelector('form').addEventListener('submit', function (event) {
-             event.preventDefault();
-
-            const formData = new FormData(event.target);
-            const jsonData = {};
-
-            for (const [key, value] of formData.entries()) {
-            jsonData[key] = value;
-        }
-
-            console.log(jsonData);
-          })}
 
         </>
         );
